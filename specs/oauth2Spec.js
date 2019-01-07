f859bdf7b2a1e040756fc1aa02f368880b288850
Douglas R. Lee <douglaslee@legalshieldcorp.com>
2019-01-07 16:43:35 -0600

    Removing wrapping describe block.

--- specs/oauth2Spec.js
+++ specs/oauth2Spec.js
@@ -30,55 +30,53 @@ describe('oauth2', function () {
       });
     });
 
-    describe('verify', function () {
-      describe('request', function () {
-        let scope, responseBody, idToken;
+    describe('request', function () {
+      let scope, responseBody, idToken;
 
-        beforeEach(function () {
-          idToken = jwt.sign({ some: 'claim' }, 'super-secret');
-          responseBody = {
-            access_token: 'some-access-token',
-            refresh_token: 'some-refresh-token',
-            id_token: idToken
-          };
-          scope = nock(options.baseURL, {
-            reqheaders: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
+      beforeEach(function () {
+        idToken = jwt.sign({ some: 'claim' }, 'super-secret');
+        responseBody = {
+          access_token: 'some-access-token',
+          refresh_token: 'some-refresh-token',
+          id_token: idToken
+        };
+        scope = nock(options.baseURL, {
+          reqheaders: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+          .post('/auth/token', {
+            grant_type: 'password',
+            scope: 'openid',
+            username: 'some-username',
+            password: 'some-password',
+            client_id: options.clientID
           })
-            .post('/auth/token', {
-              grant_type: 'password',
-              scope: 'openid',
-              username: 'some-username',
-              password: 'some-password',
-              client_id: options.clientID
-            })
-            .reply(201, responseBody);
-        });
+          .reply(201, responseBody);
+      });
 
-        describe('success', function () {
-          it('calls next with the authentication tokens', function (done) {
-            strategy._verify('some-username', 'some-password', function (user, info) {
-              expect(user).to.be.null;
-              expect(info).to.eql({
-                accessToken: 'some-access-token',
-                refreshToken: 'some-refresh-token',
-                idToken: jwt.decode(idToken)
-              });
-
-              done();
+      describe('success', function () {
+        it('calls next with the authentication tokens', function (done) {
+          strategy._verify('some-username', 'some-password', function (user, info) {
+            expect(user).to.be.null;
+            expect(info).to.eql({
+              accessToken: 'some-access-token',
+              refreshToken: 'some-refresh-token',
+              idToken: jwt.decode(idToken)
             });
+
+            done();
           });
         });
+      });
 
-        describe('failure', function () {
-          it('calls next passing false', function (done) {
-            strategy._verify('some-username', 'bad-password', function (user, info) {
-              expect(user).to.be.null;
-              expect(info).to.eql(false);
+      describe('failure', function () {
+        it('calls next passing false', function (done) {
+          strategy._verify('some-username', 'bad-password', function (user, info) {
+            expect(user).to.be.null;
+            expect(info).to.eql(false);
 
-              done();
-            });
+            done();
           });
         });
       });
